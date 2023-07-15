2b97e05dd57aa5745fa5be7f032a03de90ccd38c
Daisy Galvão <daisy.smgalvao@gmail.com>
2023-07-14 21:57:36 -0300

    Create Endereco

---
+++ CRUD/Controllers/EnderecoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using CRUD.Models;
+using System.Data.SqlClient;
+using Dapper;
+using System.Runtime.ConstrainedExecution;
+
+namespace CRUD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnderecoController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Create")]
+        public void Create(EnderecoModel enderecoModel)
+        {
+            var query = @"insert into endereco values
+                        (
+                         @Id_usuario,
+                         @Rua,
+                         @Numero,
+                         @Cep
+                        )";
+            using (var conn = new SqlConnection("Server=DAISY\\SQLEXPRESS; Database=CRUD; User Id=sa; Password=anonimo;"))
+            {
+                conn.Open();
+
+                conn.Query(query,
+                    new
+                    {
+                        Id_usuario = enderecoModel.ID_usuario,
+                        Rua = enderecoModel.rua,
+                        Numero = enderecoModel.numero,
+                        Cep = enderecoModel.cep
+                    }).FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ CRUD/Models/EnderecoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUD.Models
+{
+    public class EnderecoModel
+    {
+        public int ID { get; set; }
+        public string ID_usuario { get; set; }
+        public string rua { get; set; }
+        public string numero { get; set; }
+        public string cep { get; set; }
+
+    }
+}
